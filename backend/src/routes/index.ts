90bd1b3a1c90c949fd36fa26bd1399388ed63960
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-05-21 17:49:56 +0000

    initial commit

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,12 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.post('/user', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = { name, email };
-
-  return response.json(user);
-});
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello World!' }),
+);
 
 export default routes;
